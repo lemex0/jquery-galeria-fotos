741da933e77d7a1f8241a00f9e38027eac3af158
Pablo Leme <pablo.masterleme@gmail.com>
2023-09-19 19:30:58 -0400

    commit inicial

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+// Adiciona eventos atraves do jquery
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagen-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+        <div class="overlay-imagem-link">
+        <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+        ver imagem em tamanho real
+        </a>
+        </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(500);
+        $('#endereco-imagen-nova').val('');
+
+    })
+
+    })
